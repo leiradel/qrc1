34f545353f6f6648e9ae1126816aabfa90e7721c
Andre Leiradella <andre@leiradella.com>
2020-12-21 20:53:56 +0000

    Added sample ZX Spectrum code for Code 2 of 5

---
+++ src/spectrum/i25.asm
@@ -0,0 +1,27 @@
+; https://github.com/leiradel/qrc1
+
+qrc_pixel_up     equ qrc_pixel_up_2
+qrc_pixel_down   equ qrc_pixel_down_2
+qrc_pixel_left   equ qrc_pixel_left_2
+qrc_pixel_right  equ qrc_pixel_right_2
+qrc_invert_pixel equ qrc_invert_pixel_2
+start_pixel      equ $c0
+
+    org 24576
+
+ramtop equ $ - 1
+
+main:
+    ; Set H and L to the top-left pixel where the code will be printed, H is Y
+    ; and L is X & %11111000. The low bits of X are encoded in register C.
+column equ $ + 1
+    ld hl, 48 << 8
+
+    ; Set C to the first pixel in the byte.
+    ld c, start_pixel
+
+    ; Print it onto the screen.
+    jp i25_print
+
+include "plot.asm"
+include "../i25.asm"
---
+++ src/spectrum/i25.bas
@@ -0,0 +1,22 @@
+10 CLEAR ${ramtop}
+20 LOAD "" CODE
+30 CLS
+40 PRINT AT 0,0;"Enter message (max 16 digits)"
+50 INPUT M$
+60 LET L=LEN M$
+70 IF L=0 THEN GOTO 50
+80 PRINT AT 1,0;M$;"                                "
+90 IF L>16 THEN LET L=16
+100 LET W=L*14+16
+110 LET C=(256-W)/2
+120 POKE ${column},C AND 248
+130 LET O=${i25_message}
+140 POKE O,L
+150 FOR I=1 TO L
+160 POKE O+I,CODE M$(I)
+170 NEXT I
+180 FOR I=5 TO 10
+190 PRINT AT I,0;"                                "
+200 NEXT I
+210 RANDOMIZE USR ${main}
+220 GOTO 40
